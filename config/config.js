393d96269d5c939d517abeccb2f3d49104325184
phoenix-foxwell <analogsombra@gmail.com>
2023-11-01 12:05:38 +0530

    header added

--- config/config.js
+++ config/config.js
@@ -1,38 +1,38 @@
 //require('dotenv').config()
-// let config = {
-//     debug: true,
-//     port: 5000,
-//     secret: 'test',
-//     database: {
-//       host: 'localhost',
-//       username: 'anvi',
-//       password: 'anvi1994',
-//       database: 'daman_club',
-//       dialect: 'mysql',
-//       pool: {
-//         max: 10,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//       }
-//     }
-// }
 let config = {
-  debug: true,
-  port: 5000,
-  secret: "test",
-  database: {
-    host: "localhost",
-    username: "root",
-    password: "",
-    database: "daman_club",
-    dialect: "mysql",
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  },
-};
+    debug: true,
+    port: 5000,
+    secret: 'test',
+    database: {
+      host: 'localhost',
+      username: 'anvi',
+      password: 'anvi1994',
+      database: 'daman_club',
+      dialect: 'mysql',
+      pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    }
+}
+// let config = {
+//   debug: true,
+//   port: 5000,
+//   secret: "test",
+//   database: {
+//     host: "localhost",
+//     username: "root",
+//     password: "",
+//     database: "daman_club",
+//     dialect: "mysql",
+//     pool: {
+//       max: 10,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000,
+//     },
+//   },
+// };
 module.exports = config;
